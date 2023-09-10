ddc09f873b2c1a821d3149fc0bbff850493ad585
Mohamedsherif099 <116480277+Mohamedsherif099@users.noreply.github.com>
2023-09-10 07:58:35 +0300

    Update UserservicesApplication.java

--- userservices/src/main/java/com/jobhacker/userservices/UserservicesApplication.java
+++ userservices/src/main/java/com/jobhacker/userservices/UserservicesApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan("com.jobhacker.userservices")
+
 public class UserservicesApplication {
 
 	public static void main(String[] args) {
